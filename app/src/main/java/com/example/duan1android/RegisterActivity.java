9749c4c4e7b00581ccd95e0b50921bf1eab8b47c
Nguyen Van Duc <nduc99911@gmail.com>
2020-11-06 16:19:55 +0700

    commit

--- app/src/main/java/com/example/duan1android/RegisterActivity.java
+++ app/src/main/java/com/example/duan1android/RegisterActivity.java
@@ -3,12 +3,22 @@ package com.example.duan1android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
-
+EditText edUserName,edPassWord,edRePassWord,edHoTen,edEmail,edPhone;
+Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        edUserName=findViewById(R.id.edUserName);
+        edPassWord=findViewById(R.id.edPassWord);
+        edRePassWord=findViewById(R.id.edRePassWord);
+        edHoTen=findViewById(R.id.edHoTen);
+        edEmail=findViewById(R.id.edEmail);
+        edPhone=findViewById(R.id.edPhone);
+btnRegister=findViewById(R.id.btnRegister);
     }
 }
