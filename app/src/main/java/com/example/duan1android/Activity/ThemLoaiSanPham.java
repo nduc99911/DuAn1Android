e39a5d26b3f6f05148fd42469fce7fbce8a947ce
Nguyen Van Duc <nduc99911@gmail.com>
2020-11-10 17:20:39 +0700

    hiiiiiiiiiiiiiiiiiiiiiiiiii

--- app/src/main/java/com/example/duan1android/Activity/ThemLoaiSanPham.java
+++ app/src/main/java/com/example/duan1android/Activity/ThemLoaiSanPham.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.duan1android.R;
+
 public class ThemLoaiSanPham extends AppCompatActivity {
 
     @Override
